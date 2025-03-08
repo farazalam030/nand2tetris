8051e68a38d248e75eb5adca92da94e32fb17184
Alam, Faraz <farazalam030@live.in>
2025-03-08 18:11:02 +0000

    modify execute.sh

--- Project7/execute.sh
+++ Project7/execute.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+LOGFILE=""
+if [ ! -z "$1" ]; then
+    # echo "Usage: ./execute.sh <logfilename>"
+    LOGFILE=debug.log
+    exit 1
+fi
 
-./build.sh ./MemoryAccess/BasicTest/BasicTest.vm $1 &> debug.log
-./build.sh ./MemoryAccess/StaticTest/StaticTest.vm $1 >> debug.log
-./build.sh ./MemoryAccess/PointerTest/PointerTest.vm $1 >> debug.log
-./build.sh ./StackArithmetic/StackTest/StackTest.vm $1 >> debug.log
-./build.sh ./StackArithmetic/SimpleAdd/SimpleAdd.vm $1 >> debug.log
+
+./build.sh ./MemoryAccess/BasicTest/BasicTest.vm $1 &> ${LOGFILE}
+./build.sh ./MemoryAccess/StaticTest/StaticTest.vm $1 >> ${LOGFILE}
+./build.sh ./MemoryAccess/PointerTest/PointerTest.vm $1 >> ${LOGFILE}
+./build.sh ./StackArithmetic/StackTest/StackTest.vm $1 >> ${LOGFILE}
+./build.sh ./StackArithmetic/SimpleAdd/SimpleAdd.vm $1 >> ${LOGFILE}
