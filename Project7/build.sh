2086abe5f010cd830fb1eda5d32233323eefb029
Alam, Faraz <farazalam030@live.in>
2025-03-08 17:39:18 +0000

    modify build for debug opt as 1

--- Project7/build.sh
+++ Project7/build.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-g++ -std=c++2a -g *.cpp -o VMTranslator.o && ./VMTranslator.o $1
+DEBUG=""
+if [ "$2" == "1" ]; then
+    DEBUG="-DDEBUG"
+fi
+
+g++ ${DEBUG} -std=c++2a -g *.cpp -o VMTranslator.o && ./VMTranslator.o $1
