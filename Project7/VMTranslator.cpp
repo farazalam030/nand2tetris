87f349fb49a59bc64b054c8c1c275043f13f84e5
Alam, Faraz <farazalam030@live.in>
2025-03-08 10:53:25 +0000

    triage static Var with filename

--- Project7/VMTranslator.cpp
+++ Project7/VMTranslator.cpp
@@ -13,9 +13,10 @@ int main(int argc, char const *argv[]) {
 
   string inputVmFile = argv[1];
   string baseFileName = inputVmFile.substr(0, inputVmFile.find_last_of('.'));
-  //   string outputAsmFile = baseFileName + ".asm";
+  // string outputAsmFile = baseFileName + ".asm";
   CodeWriter codeWriter(baseFileName);
-  codeWriter.setInputVmFileName(inputVmFile);
+  string staticVar = baseFileName.substr(baseFileName.find_last_of('/') + 1);
+  codeWriter.setInputVmFileName(staticVar);
 
   Parser parser(inputVmFile);
   int lineNumber = 0;
