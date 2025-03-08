fbc7eaba827852b93453acf77d1d5d30afc9ae71
Alam, Faraz <farazalam030@live.in>
2025-03-08 18:07:29 +0000

    modify execute.sh

--- Project7/execute.sh
+++ Project7/execute.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-./build.sh ./MemoryAccess/BasicTest/BasicTest.vm 1
-./build.sh ./MemoryAccess/StaticTest/StaticTest.vm 1
-./build.sh ./MemoryAccess/PointerTest/PointerTest.vm 1
-./build.sh ./StackArithmetic/StackTest/StackTest.vm 1
-./build.sh ./StackArithmetic/SimpleAdd/SimpleAdd.vm 1
 
+./build.sh ./MemoryAccess/BasicTest/BasicTest.vm $1 &> debug.log
+./build.sh ./MemoryAccess/StaticTest/StaticTest.vm $1 >> debug.log
+./build.sh ./MemoryAccess/PointerTest/PointerTest.vm $1 >> debug.log
+./build.sh ./StackArithmetic/StackTest/StackTest.vm $1 >> debug.log
+./build.sh ./StackArithmetic/SimpleAdd/SimpleAdd.vm $1 >> debug.log
