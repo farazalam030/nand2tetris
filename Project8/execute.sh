adac54d84e76ab8f7438f7164e36a739cee8f605
Alam, Faraz <farazalam030@live.in>
2025-03-09 22:36:38 +0000

    init 2

--- Project8/execute.sh
+++ Project8/execute.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-./build.sh ./MemoryAccess/BasicTest/BasicTest.vm 1
-./build.sh ./MemoryAccess/StaticTest/StaticTest.vm 1
-./build.sh ./MemoryAccess/PointerTest/PointerTest.vm 1
-./build.sh ./StackArithmetic/StackTest/StackTest.vm 1
-./build.sh ./StackArithmetic/SimpleAdd/SimpleAdd.vm 1
+NTTLOGFILE="/dev/null"
+if ! [[ -z "$1" ]]; then
+    NTTLOGFILE=debug.log
+fi
 
+./build.sh ./MemoryAccess/BasicTest/BasicTest.vm $1 &> ${NTTLOGFILE}
+./build.sh ./MemoryAccess/StaticTest/StaticTest.vm $1 >> ${NTTLOGFILE}
+./build.sh ./MemoryAccess/PointerTest/PointerTest.vm $1 >> ${NTTLOGFILE}
+./build.sh ./StackArithmetic/StackTest/StackTest.vm $1 >> ${NTTLOGFILE}
+./build.sh ./StackArithmetic/SimpleAdd/SimpleAdd.vm $1 >> ${NTTLOGFILE}
