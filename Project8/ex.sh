320e1e49442b3b64fdaaec586b9106c6f80f4fcf
Alam, Faraz <farazalam030@live.in>
2025-03-18 01:29:28 +0000

    modify ex.sh

--- Project8/ex.sh
+++ Project8/ex.sh
@@ -2,7 +2,7 @@
 
 git clean -fdx
 ./build.sh FunctionCalls/FibonacciElement 1 &> out.log
-# ./build.sh FunctionCalls/StaticsTest 1 &> out.log
+./build.sh FunctionCalls/StaticsTest 1 &> out.log
 rm -rf /mnt/c/Users/Faraz/Desktop/FunctionCalls
 cp -ra FunctionCalls/ /mnt/c/Users/Faraz/Desktop/ -v
 
