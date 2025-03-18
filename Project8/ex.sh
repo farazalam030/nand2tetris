e40db96da678a92dd7dd5312afd3e4ab0a046ecb
Alam, Faraz <farazalam030@live.in>
2025-03-18 01:48:05 +0000

    mv ex

--- Project8/ex.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git clean -fdx
-./build.sh FunctionCalls/FibonacciElement 1 &> FibonacciElement.log
-./build.sh FunctionCalls/StaticsTest 1 &> StaticsTest.log
-./build.sh FunctionCalls/SimpleFunction/SimpleFunction.vm 1 &> SimpleFunction.vm.log
-./build.sh ProgramFlow/BasicLoop/BasicLoop.vm 1 &> ProgramFlowBasicLoop.log
-./build.sh ProgramFlow/FibonacciSeries/FibonacciSeries.vm 1 &> ProgramFlowFibonacciSeries.vm.log
-
