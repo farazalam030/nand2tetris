76f54fde34fb49693fb211043c6ba3dce602036c
Alam, Faraz <farazalam030@gmail.com>
2025-04-14 11:02:09 +0530

    v1_0 Working

--- Project10/inPython/JackCompilationEngine.py
+++ Project10/inPython/JackCompilationEngine.py
@@ -33,73 +33,75 @@ class JackCompilationEngine:
         nextt = self.tokens[self.cur_loc]
         return nextt
 
-    def add(self, token):
-        self.completeTokens.append(token)
 
     def CompileClass(self):
-        self.add(JT.NonTerminalToken('class', True))  # <class>
-        self.add(self.nextToken())  # class
-        self.add(self.nextToken())  # className
-        self.add(self.nextToken())  # {
+        self.completeTokens.append(
+            JT.NonTerminalToken('class', True))
+        self.completeTokens.append(self.nextToken())
+        self.completeTokens.append(self.nextToken())
+        self.completeTokens.append(self.nextToken())
         while self.peekToken().value in self.classVarDec:
             self.CompileClassVarDec()
         while self.peekToken().value in self.subroutineDec:
             self.CompileSubroutine()
-        self.add(self.nextToken())  # }
-        self.add(JT.NonTerminalToken('class', False))  # </class>
+        self.completeTokens.append(self.nextToken())
+        self.completeTokens.append(
+            JT.NonTerminalToken('class', False))
 
     def CompileClassVarDec(self):
-        self.add(JT.NonTerminalToken('classVarDec', True))
-        self.add(self.nextToken())  # (static | field)
-        self.add(self.nextToken())  # type
-        self.add(self.nextToken())  # varName
+        self.completeTokens.append(JT.NonTerminalToken('classVarDec', True))
+        self.completeTokens.append(self.nextToken())
+        self.completeTokens.append(self.nextToken())
+        self.completeTokens.append(self.nextToken())
         while self.peekToken().value == ',':
-            self.add(self.nextToken())  # ,
-            self.add(self.nextToken())  # varName
-        self.add(self.nextToken())  # ;
-        self.add(JT.NonTerminalToken('classVarDec', False))
+            self.completeTokens.append(self.nextToken())
+            self.completeTokens.append(self.nextToken())
+        self.completeTokens.append(self.nextToken())
+        self.completeTokens.append(JT.NonTerminalToken('classVarDec', False))
 
     def CompileSubroutine(self):
-        self.add(JT.NonTerminalToken('subroutineDec', True))
-        self.add(self.nextToken())  # (constructor | function | method)
-        self.add(self.nextToken())  # (void | type)
-        self.add(self.nextToken())  # subroutineName
-        self.add(self.nextToken())  # '('
+        self.completeTokens.append(JT.NonTerminalToken('subroutineDec', True))
+
+        self.completeTokens.append(self.nextToken())
+        self.completeTokens.append(self.nextToken())
+        self.completeTokens.append(self.nextToken())
+        self.completeTokens.append(self.nextToken())
         self.CompileParameterList()
-        self.add(self.nextToken())  # ')'
-        self.add(JT.NonTerminalToken('subroutineBody', True))
-        self.add(self.nextToken())  # '{'
+        self.completeTokens.append(self.nextToken())
+        self.completeTokens.append(JT.NonTerminalToken('subroutineBody', True))
+        self.completeTokens.append(self.nextToken())
         while self.peekToken().value == 'var':
             self.CompileVarDec()
         self.CompileStatements()
-        self.add(self.nextToken())  # '}'
-        self.add(JT.NonTerminalToken('subroutineBody', False))
-        self.add(JT.NonTerminalToken('subroutineDec', False))
+        self.completeTokens.append(self.nextToken())
+        self.completeTokens.append(
+            JT.NonTerminalToken('subroutineBody', False))
+        self.completeTokens.append(JT.NonTerminalToken('subroutineDec', False))
 
     def CompileParameterList(self):
-        self.add(JT.NonTerminalToken('parameterList', True))
+        self.completeTokens.append(JT.NonTerminalToken('parameterList', True))
         if self.peekToken().value != ')':
-            self.add(self.nextToken())  # type
-            self.add(self.nextToken())  # varName
+            self.completeTokens.append(self.nextToken())
+            self.completeTokens.append(self.nextToken())
         while self.peekToken().value == ',':
-            self.add(self.nextToken())  # ,
-            self.add(self.nextToken())  # type
-            self.add(self.nextToken())  # varName
-        self.add(JT.NonTerminalToken('parameterList', False))
+            self.completeTokens.append(self.nextToken())
+            self.completeTokens.append(self.nextToken())
+            self.completeTokens.append(self.nextToken())
+        self.completeTokens.append(JT.NonTerminalToken('parameterList', False))
 
     def CompileVarDec(self):
-        self.add(JT.NonTerminalToken('varDec', True))
-        self.add(self.nextToken())  # var
-        self.add(self.nextToken())  # type
-        self.add(self.nextToken())  # varName
+        self.completeTokens.append(JT.NonTerminalToken('varDec', True))
+        self.completeTokens.append(self.nextToken())
+        self.completeTokens.append(self.nextToken())
+        self.completeTokens.append(self.nextToken())
         while self.peekToken().value == ',':
-            self.add(self.nextToken())  # ,
-            self.add(self.nextToken())  # varName
-        self.add(self.nextToken())  # ;
-        self.add(JT.NonTerminalToken('varDec', False))
+            self.completeTokens.append(self.nextToken())
+            self.completeTokens.append(self.nextToken())
+        self.completeTokens.append(self.nextToken())
+        self.completeTokens.append(JT.NonTerminalToken('varDec', False))
 
     def CompileStatements(self):
-        self.add(JT.NonTerminalToken('statements', True))
+        self.completeTokens.append(JT.NonTerminalToken('statements', True))
         while self.peekToken().value in self.statements:
             v = self.peekToken().value
             if v == 'if':
@@ -112,116 +114,120 @@ class JackCompilationEngine:
                 self.CompileDo()
             else:
                 self.CompileReturn()
-        self.add(JT.NonTerminalToken('statements', False))
+        self.completeTokens.append(JT.NonTerminalToken('statements', False))
 
     def CompileDo(self):
-        self.add(JT.NonTerminalToken('doStatement', True))
-        self.add(self.nextToken())  # do
-        self.add(self.nextToken())  # subroutineCall
+        self.completeTokens.append(JT.NonTerminalToken('doStatement', True))
+        self.completeTokens.append(self.nextToken())
+        self.completeTokens.append(self.nextToken())
         if self.peekToken().value == '(':
-            self.add(self.nextToken())  # '('
+            self.completeTokens.append(self.nextToken())
             self.CompileExpressionList()
-            self.add(self.nextToken())  # ')'
+            self.completeTokens.append(self.nextToken())
         elif self.peekToken().value == '.':
-            self.add(self.nextToken())  # '.'
-            self.add(self.nextToken())  # subroutineName
-            self.add(self.nextToken())  # (
+            self.completeTokens.append(self.nextToken())
+            self.completeTokens.append(self.nextToken())
+            self.completeTokens.append(self.nextToken())
             self.CompileExpressionList()
-            self.add(self.nextToken())  # )
-        self.add(self.nextToken())  # ;
-        self.add(JT.NonTerminalToken('doStatement', False))
+            self.completeTokens.append(self.nextToken())
+        self.completeTokens.append(self.nextToken())
+        self.completeTokens.append(JT.NonTerminalToken('doStatement', False))
 
     def CompileLet(self):
-        self.add(JT.NonTerminalToken('letStatement', True))
-        self.add(self.nextToken())  # let
-        self.add(self.nextToken())  # varName
+        self.completeTokens.append(JT.NonTerminalToken('letStatement', True))
+        self.completeTokens.append(self.nextToken())
+        self.completeTokens.append(self.nextToken())
         if self.peekToken().value == '[':
-            self.add(self.nextToken())  # '['
+            self.completeTokens.append(self.nextToken())
             self.CompileExpression()
-            self.add(self.nextToken())  # ']'
-        self.add(self.nextToken())  # =
+            self.completeTokens.append(self.nextToken())
+        self.completeTokens.append(self.nextToken())
         self.CompileExpression()
-        self.add(self.nextToken())  # ;
-        self.add(JT.NonTerminalToken('letStatement', False))
+        self.completeTokens.append(self.nextToken())
+        self.completeTokens.append(JT.NonTerminalToken('letStatement', False))
 
     def CompileWhile(self):
-        self.add(JT.NonTerminalToken('whileStatement', True))
-        self.add(self.nextToken())  # while
-        self.add(self.nextToken())  # (
+        self.completeTokens.append(JT.NonTerminalToken('whileStatement', True))
+        self.completeTokens.append(self.nextToken())
+        self.completeTokens.append(self.nextToken())
         self.CompileExpression()
-        self.add(self.nextToken())  # )
-        self.add(self.nextToken())  # {
+        self.completeTokens.append(self.nextToken())
+        self.completeTokens.append(self.nextToken())
         self.CompileStatements()
-        self.add(self.nextToken())  # }
-        self.add(JT.NonTerminalToken('whileStatement', False))
+        self.completeTokens.append(self.nextToken())
+        self.completeTokens.append(
+            JT.NonTerminalToken('whileStatement', False))
 
     def CompileReturn(self):
-        self.add(JT.NonTerminalToken('returnStatement', True))
-        self.add(self.nextToken())  # return
+        self.completeTokens.append(
+            JT.NonTerminalToken('returnStatement', True))
+        self.completeTokens.append(self.nextToken())
         if self.peekToken().value != ';':
             self.CompileExpression()
-        self.add(self.nextToken())  # ;
-        self.add(JT.NonTerminalToken('returnStatement', False))
+        self.completeTokens.append(self.nextToken())
+        self.completeTokens.append(
+            JT.NonTerminalToken('returnStatement', False))
 
     def CompileIf(self):
-        self.add(JT.NonTerminalToken('ifStatement', True))
-        self.add(self.nextToken())  # if
-        self.add(self.nextToken())  # (
+        self.completeTokens.append(JT.NonTerminalToken('ifStatement', True))
+        self.completeTokens.append(self.nextToken())
+        self.completeTokens.append(self.nextToken())
         self.CompileExpression()
-        self.add(self.nextToken())  # )
-        self.add(self.nextToken())  # {
+        self.completeTokens.append(self.nextToken())
+        self.completeTokens.append(self.nextToken())
         self.CompileStatements()
-        self.add(self.nextToken())  # }
+        self.completeTokens.append(self.nextToken())
         if self.peekToken().value == 'else':
-            self.add(self.nextToken())  # else
-            self.add(self.nextToken())  # {
+            self.completeTokens.append(self.nextToken())
+            self.completeTokens.append(self.nextToken())
             self.CompileStatements()
-            self.add(self.nextToken())  # }
-        self.add(JT.NonTerminalToken('ifStatement', False))
+            self.completeTokens.append(self.nextToken())
+        self.completeTokens.append(JT.NonTerminalToken('ifStatement', False))
 
     def CompileExpression(self):
-        self.add(JT.NonTerminalToken('expression', True))
+        self.completeTokens.append(JT.NonTerminalToken('expression', True))
         self.CompileTerm()
         while self.peekToken().value in self.binary_op:
-            self.add(self.nextToken())  # op
+            self.completeTokens.append(self.nextToken())
             self.CompileTerm()
-        self.add(JT.NonTerminalToken('expression', False))
+        self.completeTokens.append(JT.NonTerminalToken('expression', False))
 
     def CompileTerm(self):
-        self.add(JT.NonTerminalToken('term', True))
+        self.completeTokens.append(JT.NonTerminalToken('term', True))
         if self.peekToken().value == '(':
-            self.add(self.nextToken())  # (
+            self.completeTokens.append(self.nextToken())
             self.CompileExpression()
-            self.add(self.nextToken())  # )
+            self.completeTokens.append(self.nextToken())
         elif self.peekToken().value in self.unary_op:
-            self.add(self.nextToken())  # - or ~
+            self.completeTokens.append(self.nextToken())
             self.CompileTerm()
         else:
-            self.add(self.nextToken())
+            self.completeTokens.append(self.nextToken())
             if self.peekToken().value == '[':
-                self.add(self.nextToken())  # '['
+                self.completeTokens.append(self.nextToken())
                 self.CompileExpression()
-                self.add(self.nextToken())  # ']'
+                self.completeTokens.append(self.nextToken())
             elif self.peekToken().value == '(':
-                self.add(self.nextToken())  # '('
+                self.completeTokens.append(self.nextToken())
                 self.CompileExpressionList()
-                self.add(self.nextToken())  # ')'
+                self.completeTokens.append(self.nextToken())
             elif self.peekToken().value == '.':
-                self.add(self.nextToken())  # '.'
-                self.add(self.nextToken())  # subroutineName
-                self.add(self.nextToken())  # (
+                self.completeTokens.append(self.nextToken())
+                self.completeTokens.append(self.nextToken())
+                self.completeTokens.append(self.nextToken())
                 self.CompileExpressionList()
-                self.add(self.nextToken())  # )
-        self.add(JT.NonTerminalToken('term', False))
+                self.completeTokens.append(self.nextToken())
+        self.completeTokens.append(JT.NonTerminalToken('term', False))
 
     def CompileExpressionList(self):
-        self.add(JT.NonTerminalToken('expressionList', True))
+        self.completeTokens.append(JT.NonTerminalToken('expressionList', True))
         if self.peekToken().value != ')':
             self.CompileExpression()
             while self.peekToken().value == ',':
-                self.add(self.nextToken())  # ,
+                self.completeTokens.append(self.nextToken())
                 self.CompileExpression()
-        self.add(JT.NonTerminalToken('expressionList', False))
+        self.completeTokens.append(
+            JT.NonTerminalToken('expressionList', False))
 
     def save_file(self):
         with open(self.output_file, 'w') as file:
